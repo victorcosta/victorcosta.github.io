e087b40a6c37a26107c18f771552785bed5ab506
Joao Dias <joao.dias@midway.com.br>
2020-06-15 00:43:06 -0300

    Updates

---
+++ precache-manifest.5d798c71eed2e4f5520d71b90238a2cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeeca19eca88f9bf630324026a769d07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9c9ff12f0a6f5e8c4c9",
+    "url": "/static/js/2.ff6f140e.chunk.js"
+  },
+  {
+    "revision": "e8fb15c8fefb22bc0e69708b5de57c24",
+    "url": "/static/js/2.ff6f140e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86613658e19acc5b38e3",
+    "url": "/static/js/main.a2676f2d.chunk.js"
+  },
+  {
+    "revision": "e0c5a4a5e778671927e0",
+    "url": "/static/js/runtime-main.01bf120f.js"
+  },
+  {
+    "revision": "6b72f1dca71ad90c32add6b9e2dc61cd",
+    "url": "/static/media/bg.6b72f1dc.png"
+  },
+  {
+    "revision": "b3b41a5c019148909e1b567ab73f02f1",
+    "url": "/static/media/logo.b3b41a5c.svg"
+  },
+  {
+    "revision": "10173ff6b76de8d1ebcc4908484dca31",
+    "url": "/static/media/profile.10173ff6.png"
+  }
+]);
